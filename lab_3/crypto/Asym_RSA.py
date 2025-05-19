4ece9c23070bedefda3b8730ccb6495a3aaffa07
илья <Oksimonov@mail.ru>
2025-05-19 11:59:54 +0300

    Инициализация класса RSACipher + реализация функции генерации ключей

--- lab_3/crypto/Asym_RSA.py
+++ lab_3/crypto/Asym_RSA.py
@@ -1,1 +1,16 @@
-print("dd")
+from cryptography.hazmat.primitives.asymmetric import rsa  # Для генерации ключей RSA
+
+
+class RSACipher:
+    def __init__(self, key_size=2048):
+        self.key_size = key_size
+
+    def generate_keys(self):
+        """Generate RSA key pair"""
+        private_key = rsa.generate_private_key(
+            public_exponent=65537,
+            key_size=self.key_size,
+            backend=default_backend()
+        )
+        public_key = private_key.public_key()
+        return private_key, public_key

c217c94495469d9c4af825700212a8b27b0f696e
илья <Oksimonov@mail.ru>
2025-05-19 11:18:47 +0300

    Инициализация класса BlowfishCipher

--- lab_3/crypto/Sym_Blowfish.py
+++ lab_3/crypto/Sym_Blowfish.py
@@ -1,1 +1,8 @@
-print("dddd")
+import os
+
+class BlowfishCipher:
+    def __init__(self, key_length=448):
+        if key_length < 32 or key_length > 448:
+            raise ValueError("Blowfish key length must be between 32 and 448 bits")
+        self.key_length = key_length
+        self.block_size = 64  # Blowfish block size in bits (8 bytes)
